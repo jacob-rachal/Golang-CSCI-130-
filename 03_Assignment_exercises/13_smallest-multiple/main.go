d7d2149fe2552dc97bd22bc4eba6eb094df680aa
jacob-rachal <jacob_rachal@mail.fresnostate.edu>
2016-03-25 09:47:40 -0700

    I'll get to this later.
    So YES, I am going to get 100% for this group of assignments.

---
+++ 03_Assignment_exercises/13_smallest-multiple/main.go
@@ -0,0 +1,14 @@
+// Jacob Rachal 3-5-16
+// Fresno State
+// projecteuler.net problem #5
+/*
+
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+*/
+package main //_3_smallest_multiple
+import "fmt"
+
+func main(){}
