3aa131536952332c5bfb897a9c84c0e88d715c87
jacob-rachal <jacob_rachal@mail.fresnostate.edu>
2016-03-30 18:25:22 -0700

    First push, no changes to code from what is given to us.
    I’m going to try making it a habit of committing updates every step of
    the way from now on.

---
+++ 08_Assignment_uuid-in-url/model.go
@@ -0,0 +1,10 @@
+// Jacob Rachal 3-30-16 Wed.
+// Fresno State CSCI 130
+//package _8_Assignment_uuid_in_url
+package mem
+
+type model struct {
+	Name     string
+	State    bool
+	Pictures []string
+}
