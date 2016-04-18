828316bb163b698c324c4e02f3b5366b95a4533b
jacob-rachal <jacob_rachal@mail.fresnostate.edu>
2016-04-17 22:19:17 -0700

    I think that's correct...
    …then again, with how my luck has been going this semester, I could be
    wrong. I might clean this up at a later date.

--- 11_Assignment_gcs-query-delimiter/main.go
+++ 11_Assignment_gcs-query-delimiter/main.go
@@ -5,7 +5,106 @@
 // the functionality of the query's delimeter field.
 package main//_1_Assignment_gcs_query_delimiter
 import(
-
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"google.golang.org/cloud/storage"
+	"io"
+	"net/http"
 
 )
 //other things here
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+const gcsBucket = "jacobs_130_project-1_bucket-1.com" //this right?
+
+type demo struct {
+	ctx    context.Context
+	res    http.ResponseWriter
+	bucket *storage.BucketHandle
+	client *storage.Client
+}
+
+func handler(res http.ResponseWriter, req *http.Request) {
+
+	if req.URL.Path != "/" {
+		http.NotFound(res, req)
+		return
+	}
+
+	ctx := appengine.NewContext(req)
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Errorf(ctx, "ERROR handler NewClient: ", err)
+		return
+	}
+	defer client.Close()
+
+	d := &demo{
+		ctx:    ctx,
+		res:    res,
+		client: client,
+		bucket: client.Bucket(gcsBucket),
+	}
+
+	d.createFiles()
+	d.listFiles()
+	io.WriteString(d.res, "\nFILE NAMES WITH DELIMITER QUERY ( Delimeter: / )\n")
+	d.listDelim()
+
+}
+func (d *demo) listDelim() {
+
+	query := &storage.Query{
+		Delimiter: "/",
+	}
+
+	objs, err := d.bucket.List(d.ctx, query)
+	if err != nil {
+		log.Errorf(d.ctx, "listBucketDirMode: unable to list bucket %q: %v", gcsBucket, err)
+		return
+	}
+
+	for _, obj := range objs.Results {
+		fmt.Fprintf(d.res, "%v\n", obj.Name)
+	}
+
+	fmt.Fprintf(d.res, "\n Prefixes ( storage.ObjectList Prefixes )\n%v", objs.Prefixes)
+}
+func (d *demo) listFiles() {
+	io.WriteString(d.res, "ALL FILE NAMES\n")
+	objs, err := d.bucket.List(d.ctx, nil)
+	if err != nil {
+		log.Errorf(d.ctx, "%v", err)
+		return
+	}
+
+	for _, obj := range objs.Results {
+		io.WriteString(d.res, obj.Name+"\n")
+	}
+}
+
+func (d *demo) createFiles() {
+	io.WriteString(d.res, "\nCreating some files for listbucket...\n")
+	for _, n := range []string{"foo1", "foo2", "bar", "bar/1", "bar/2", "boo/", "foo/boo/foo3", "foo/boo/foo/4", "boo/yah5", "compadre/amigo/diaz6", "compadre/luego/hasta7", "bar/nonce/8", "bar/nonce/9", "bar/nonce/compadre/10", "bar/nonce/compadre/11"} {
+		d.createFile(n)
+	}
+}
+func (d *demo) createFile(fileName string) {
+
+	wc := d.bucket.Object(fileName).NewWriter(d.ctx)
+	wc.ContentType = "text/plain"
+
+	if _, err := wc.Write([]byte("abcde\n")); err != nil {
+		log.Errorf(d.ctx, "createFile: unable to write data to bucket %q, file %q: %v", gcsBucket, fileName, err)
+		return
+	}
+	if err := wc.Close(); err != nil {
+		log.Errorf(d.ctx, "createFile: unable to close bucket %q, file %q: %v", gcsBucket, fileName, err)
+		return
+	}
+}
